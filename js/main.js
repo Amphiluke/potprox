c447df0fe8b44f12e70bbf3cc4e5c8a441741747
Amphiluke <amphiluke@gmail.com>
2017-08-19 19:39:29 +0600

    Process Winbond input files

--- js/main.js
+++ js/main.js
@@ -4,6 +4,17 @@
     let Chart = global.Chart,
         potprox = global.potprox;
 
+    let utils = {
+        readFile(file) {
+            return new Promise((resolve, reject) => {
+                let reader = new FileReader();
+                reader.addEventListener("load", () => resolve(reader.result));
+                reader.addEventListener("error", () => reject(reader.error));
+                reader.readAsText(file);
+            });
+        }
+    };
+
     let dataCtrl = {
         parseCSV(csv) {
             csv = csv.trim();
@@ -17,6 +28,33 @@
                 data.push({x, y});
             }
             return data;
+        },
+
+        winbondToCSV(kul, sum1, sum2) {
+            let kulLines = kul.trim().split(/\r?\n/);
+            let sum1Lines = sum1.trim().split(/\r?\n/);
+            let sum2Lines = sum2.trim().split(/\r?\n/);
+            if (kulLines.length !== sum1Lines.length || kulLines.length !== sum2Lines.length) {
+                throw new Error("Winbond files contain mismatched or corrupted data");
+            }
+            let splitRE = /\s+/;
+            let csvLines = kulLines.map((kulLine, lineIndex) => {
+                let columns = kulLine.trim().split(splitRE);
+                let distance = Number(columns[0]);
+                let energy = Number(columns[1]);
+                columns = sum1Lines[lineIndex].trim().split(splitRE);
+                if (Number(columns[0]) !== distance) {
+                    throw new Error("Winbond files contain mismatched or corrupted data");
+                }
+                energy += Number(columns[columns.length - 1]);
+                columns = sum2Lines[lineIndex].trim().split(splitRE);
+                if (Number(columns[0]) !== distance) {
+                    throw new Error("Winbond files contain mismatched or corrupted data");
+                }
+                energy += Number(columns[columns.length - 1]);
+                return `${distance},${energy.toFixed(8)}`; // sum files contain energy output with 8 decimal digits
+            });
+            return csvLines.join("\n");
         },
 
         toPotprox(data) {
@@ -106,13 +144,7 @@
     };
 
     let uiCtrl = {
-        ui: {
-            form: document.getElementById("ctrl-form"),
-            file: document.getElementById("file"),
-            data: document.getElementById("data"),
-            potential: document.getElementById("potential"),
-            graph: document.getElementById("graph")
-        },
+        ui: new Map([...document.body.querySelectorAll("[id]")].map(el => [`#${el.id}`, el])),
 
         init() {
             this.addEventListeners();
@@ -120,46 +152,79 @@
         },
 
         updateChart() {
-            let userData = dataCtrl.parseCSV(this.ui.data.value);
-            let potentialType = this.ui.potential.value;
+            let ui = this.ui;
+            let userData = dataCtrl.parseCSV(ui.get("#data").value);
+            let potentialType = ui.get("#potential").value;
             let {potential, data} = dataCtrl.approximate(userData, potentialType);
             this.updateParamSlots(potentialType, potential);
             if (chartCtrl.chart) {
                 chartCtrl.update(userData, data);
             } else {
-                chartCtrl.createChart(this.ui.graph.getContext("2d"), userData, data);
+                chartCtrl.createChart(ui.get("#graph").getContext("2d"), userData, data);
             }
         },
 
         updateParamSlots(potentialType, data = {}) {
             let selector = potentialType ? `[data-potential="${potentialType}"]` : "[data-potential]";
-            let paramSlots = this.ui.form.querySelectorAll(`.result${selector} [data-param]`);
+            let paramSlots = this.ui.get("#ctrl-form").querySelectorAll(`.result${selector} [data-param]`);
             [...paramSlots].forEach(slot => slot.textContent = data[slot.getAttribute("data-param")] || "N/A");
         },
 
-        addEventListeners() {
-            let ui = this.ui;
-            ui.file.addEventListener("change", this.fileChangeHandler.bind(this), false);
-            ui.data.addEventListener("change", () => this.updateParamSlots(), false);
-            ui.potential.addEventListener("change", this.potentialChangeHandler.bind(this), false);
-            ui.form.addEventListener("submit", this.submitHandler.bind(this), false);
+        addEventListener(elRef, event, method) {
+            let el = (typeof elRef === "string") ? this.ui.get(elRef) : elRef;
+            let listener = (typeof method === "string") ? this[method].bind(this) : method;
+            el.addEventListener(event, listener, false);
         },
 
-        fileChangeHandler(e) {
-            let file = e.target.files[0];
+        addEventListeners() {
+            this.addEventListener("#file-tab-ctrl", "click", "clickTabCtrlHandler");
+            this.addEventListener("#file-csv", "change", "fileCSVChangeHandler");
+            this.addEventListener(this.ui.get("#file-tab-ctrl").querySelector(".tab-sheet[data-format='winbond']"),
+                "change", "fileWinbondChangeHandler");
+            this.addEventListener("#data", "change", () => this.updateParamSlots());
+            this.addEventListener("#potential", "change", "potentialChangeHandler");
+            this.addEventListener("#ctrl-form", "submit", "submitHandler");
+        },
+
+        clickTabCtrlHandler({target}) {
+            if (!target.classList.contains("tab") || target.classList.contains("tab-active")) {
+                return;
+            }
+            [...this.ui.get("#file-tab-ctrl").querySelectorAll(".tab")]
+                .forEach(el => el.classList.toggle("tab-active", el === target));
+            let format = target.dataset.format;
+            [...this.ui.get("#file-tab-ctrl").querySelectorAll(".tab-sheet")]
+                .forEach(el => el.classList.toggle("hidden", el.dataset.format !== format));
+        },
+
+        fileCSVChangeHandler({target}) {
+            let file = target.files[0];
             if (!file) {
                 return;
             }
-            let reader = new FileReader();
-            reader.addEventListener("load", () => this.ui.data.value = reader.result);
-            reader.addEventListener("error", () => console.error(reader.error));
-            reader.readAsText(file);
+            utils.readFile(file)
+                .then(csv => this.ui.get("#data").value = csv)
+                .catch(error => console.error(error));
             this.updateParamSlots();
         },
 
-        potentialChangeHandler(e) {
-            let type = e.target.value;
-            let dependents = this.ui.form.querySelectorAll("[data-potential]");
+        fileWinbondChangeHandler() {
+            let ui = this.ui;
+            let kulFile = ui.get("#file-kul").files[0];
+            let sum1File = ui.get("#file-sum1").files[0];
+            let sum2File = ui.get("#file-sum2").files[0];
+            if (!kulFile || !sum1File || !sum2File) {
+                return;
+            }
+            Promise.all([utils.readFile(kulFile), utils.readFile(sum1File), utils.readFile(sum2File)])
+                .then(([kul, sum1, sum2]) => this.ui.get("#data").value = dataCtrl.winbondToCSV(kul, sum1, sum2))
+                .catch(error => console.error(error));
+            this.updateParamSlots();
+        },
+
+        potentialChangeHandler({target}) {
+            let type = target.value;
+            let dependents = this.ui.get("#ctrl-form").querySelectorAll("[data-potential]");
             [...dependents].forEach(el => el.classList.toggle("hidden", el.getAttribute("data-potential") !== type));
         },
 
