97e16eb09e62e07c11f9f5794a9330169678f5c8
Amphiluke <amphiluke@gmail.com>
2017-10-25 22:25:57 +0700

    Fix up test failure induced by the differences in Number.EPSILON on different platforms

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -1,6 +1,8 @@
+const LIM_ERR = 10 ** Math.ceil(Math.log10(Number.EPSILON));
+
 let utils = {
     equal(num1, num2) {
-        return Math.abs(num1 - num2) <= Number.EPSILON;
+        return Math.abs(num1 - num2) < LIM_ERR;
     }
 };
 
